e6c728ae55a47a1226ff97742db6f3e82a9d5f94
Antti S. Lankila <alankila@gmail.com>
2013-03-30 21:02:38 +0200

    Some ugly hacks further proving the concept
    - try every glyph in the font in order to optimize it

--- glyph-positional-optimization/ft-glyph-aligner.c
+++ glyph-positional-optimization/ft-glyph-aligner.c
@@ -48,7 +48,7 @@ int score_glyph(FT_GlyphSlot glyph, int dx, int dy) {
     return score;
 }*/
 
-void draw_glyph(FT_GlyphSlot glyph, int dx, int dy) {
+int draw_glyph(FT_GlyphSlot glyph, int dx, int dy) {
     FT_Glyph copy;
     FT_Error error;
 
@@ -66,112 +66,81 @@ void draw_glyph(FT_GlyphSlot glyph, int dx, int dy) {
     draw_glyph_bitmap(bitmapcopy->bitmap);
     FT_Done_Glyph(copy);
     fprintf(stdout, "\n");
+    return score;
 }
 
 int main(int argc, char **argv) {
-  if (argc != 3) {
-    fprintf(stderr, "Usage: %s font_name size_in_px\n", argv[0]);
-    return 1;
-  }
+    if (argc != 3) {
+	fprintf(stderr, "Usage: %s font_file size_in_px\n", argv[0]);
+	return 1;
+    }
 
-  char *font_name = argv[1];
-  int size_in_px = atoi(argv[2]);
-  char* letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  int letters_len = strlen(letters);
+    char *font_name = argv[1];
+    int size_in_px = atoi(argv[2]);
 
-  FT_Library library;
-  FT_Error error;
+    FT_Library library;
+    FT_Error error;
 
-  error = FT_Init_FreeType(&library);
-  if (error) {
-    fprintf(stderr, "FT init freetype: error %d\n", error);
-    return 1;
-  }
+    error = FT_Init_FreeType(&library);
+    if (error) {
+	fprintf(stderr, "FT init freetype: error %d\n", error);
+	return 1;
+    }
 
-  FT_Face face;
-  error = FT_New_Face(library, font_name, 0, &face);
-  if (error) {
-    fprintf(stderr, "FT new face: error %d\n", error);
-    return 1;
-  }
+    FT_Face face;
+    error = FT_New_Face(library, font_name, 0, &face);
+    if (error) {
+	fprintf(stderr, "FT new face: error %d\n", error);
+	return 1;
+    }
 
-  error = FT_Set_Pixel_Sizes(face, size_in_px, size_in_px);
-  if (error) {
-    fprintf(stderr, "FT set pixel sizes: error %d\n", error);
-    return 1;
-  }
+    error = FT_Set_Pixel_Sizes(face, size_in_px, size_in_px);
+    if (error) {
+	fprintf(stderr, "FT set pixel sizes: error %d\n", error);
+	return 1;
+    }
 
-  /* Perform a 2D scan of the surface looking for optimal positioning in
-   * 5 * 9 = 45 iterations */
-  int step = 16;
-  int dx = 0;
-  int dy = 0;
-  while (step != 0) {
-    int bestscore = -0x7ffffff;
-    int besti = 0;
-    for (int i = 0; i < 9; i ++) {
-      /* Generate adjustment coordinates:
-       *
-       * 0 1 2
-       * 3 4 5
-       * 6 7 8
-       *
-       * In theory we could cache 4, because last iteration's 4 score
-       * could be kept in bestscore.
-       */
-      int adjx = ((i % 3) - 1) * step;
-      int adjy = ((i / 3) - 1) * step;
+    /* Scan for optimal y offset */
+    int bestdy = 0;
+    int bestscore = -0x7fffffff;
+    for (int dy = -32; dy < 32; dy ++) {
+        FT_Vector position = { 0, dy };
+        FT_Set_Transform(face, 0, &position);
+        int score = 0;
+        for (int glyph_index = 1; glyph_index < face->num_glyphs; glyph_index ++) {
+	    error = FT_Load_Glyph(face, glyph_index, FT_LOAD_NO_AUTOHINT | FT_LOAD_NO_HINTING | FT_LOAD_NO_BITMAP);
+            if (error) {
+                fprintf(stderr, "FT load glyph: error %d\n", error);
+                return 1;
+            }
 
-      int score = 0;
-      FT_Vector position = { dx + adjx, dy + adjy };
-      FT_Set_Transform(face, 0, &position);
-      for (int idx = 0; idx < letters_len; idx ++) {
-	int glyph_index = FT_Get_Char_Index(face, letters[idx]);
-	if (!glyph_index) {
-	  fprintf(stderr, "FT get char index: no glyph found for codepoint %d\n", letters[idx]);
-	  return 1;
-	}
-
-        error = FT_Load_Glyph(face, glyph_index, FT_LOAD_NO_AUTOHINT | FT_LOAD_NO_HINTING | FT_LOAD_NO_BITMAP);
-        if (error) {
-          fprintf(stderr, "FT load glyph: error %d\n", error);
-          return 1;
+	    error = FT_Render_Glyph(face->glyph, FT_RENDER_MODE_NORMAL);
+	    if (error) {
+                fprintf(stderr, "FT render glyph: error %d\n", error);
+                return 1;
+	    }
+            score += score_glyph_bitmap(face->glyph->bitmap);
         }
 
-	error = FT_Render_Glyph(face->glyph, FT_RENDER_MODE_NORMAL);
+        if (score > bestscore) {
+            bestscore = score;
+	    bestdy = dy;
+        }
+    }
+
+
+    FT_Set_Transform(face, NULL, NULL);
+    int defaultscore = 0;
+    int adjustedscore = 0;
+    for (int glyph_index = 1; glyph_index < face->num_glyphs; glyph_index ++) {
+	error = FT_Load_Glyph(face, glyph_index, FT_LOAD_NO_AUTOHINT | FT_LOAD_NO_HINTING | FT_LOAD_NO_BITMAP);
 	if (error) {
-          fprintf(stderr, "FT render glyph: error %d\n", error);
-          return 1;
+	    fprintf(stderr, "FT load glyph: error %d\n", error);
+	    return 1;
 	}
-        score += score_glyph_bitmap(face->glyph->bitmap);
-      }
 
-      if (score > bestscore) {
-        bestscore = score;
-        besti = i;
-      }
+	defaultscore += draw_glyph(face->glyph, 0, 0);
+	adjustedscore += draw_glyph(face->glyph, 0, bestdy);
     }
-
-    dx += ((besti % 3) - 1) * step;
-    dy += ((besti / 3) - 1) * step;
-    step >>= 1;
-  }
-
-  FT_Set_Transform(face, NULL, NULL);
-  for (int idx = 0; idx < letters_len; idx ++) {
-    int glyph_index = FT_Get_Char_Index(face, letters[idx]);
-    if (!glyph_index) {
-      fprintf(stderr, "FT get char index: no glyph found for codepoint %d\n", letters[idx]);
-      return 1;
-    }
-
-    error = FT_Load_Glyph(face, glyph_index, FT_LOAD_NO_AUTOHINT | FT_LOAD_NO_HINTING | FT_LOAD_NO_BITMAP);
-    if (error) {
-      fprintf(stderr, "FT load glyph: error %d\n", error);
-      return 1;
-    }
-
-    draw_glyph(face->glyph, 0, 0);
-    draw_glyph(face->glyph, dx, dy);
-  }
+    fprintf(stdout, "Total change: %d -> %d\n", defaultscore, adjustedscore); 
 }
