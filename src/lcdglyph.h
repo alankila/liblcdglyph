6e996872112fe95ba622cb98ffdca48f383389e4
Antti S. Lankila <alankila@gmail.com>
2013-04-02 09:13:16 +0300

    Get rid of filtering capability for now
    I will trust FreeType to do this correctly. All the user has to do is replace
    each alpha via the gable based on the known foreground level.

--- src/lcdglyph.h
+++ src/lcdglyph.h
@@ -7,30 +7,4 @@ uint8_t *lcdg_get_default_table();
 
 void lcdg_build_table(uint8_t *table, float *error, uint8_t bg_start, uint8_t bg_end);
 
-struct lcdg_reader;
-
-typedef uint8_t lcdg_reader_func_t(struct lcdg_reader *reader, int32_t x, int32_t y);
-
-typedef struct lcdg_reader {
-	int32_t width;
-	int32_t height;
-	int32_t stride;
-	void *data;
-	lcdg_reader_func_t *read;
-} lcdg_reader_t;
-
-struct lcdg_writer;
-
-typedef void lcdg_writer_func_t(struct lcdg_writer *writer, int32_t x, int32_t y, uint8_t value);
-
-typedef struct lcdg_writer {
-	int32_t width; /* width is in subpixels, i.e multiplied by 3. */
-	int32_t height; /* height is in pixel rows */
-	int32_t stride;
-	void *data;
-	lcdg_writer_func_t *write;
-} lcdg_writer_t;
-
-void lcdg_filter_horizontal(lcdg_reader_t *reader, lcdg_writer_t *writer, uint8_t *alpha_correct_table[3], uint8_t foreground[3], uint8_t dark_glyph_enhancement);
-
 #endif
